0f3010834aa53eb4936afa26f92219d37ce7cbf8
Eugen Martin <eugeniusmartinus@gmail.com>
2024-07-16 12:34:14 +0200

    support website urls as well

--- src/toolCallingAgent.ts
+++ src/toolCallingAgent.ts
@@ -17,8 +17,8 @@ You will be given the app store url of the app.
 Tasks:
 - get the info about the app from the url you're given
 - from the app info compile a list of search terms that you can use to find relevant user questions like "How do I do X?", "What's the best app to do X?" where X is some of the use cases of the app.
-- use the tools to find relevant posts on reddit.com using the search terms, return 5 posts per search term. only search for posts on reddit and no other sites.
-- select only posts that are relevant to the app and provide a list of those posts
+- use the tools to find relevant posts on reddit.com using the search terms, return 10 posts per search term. Only search for posts on reddit and no other sites.
+- rank the posts by how relevant they are to the app and return the top 5 posts per search term.
 
 Respond with the list of search terms you have used and with the list of relevant posts for each search term.
 Respond in markdown format.
@@ -50,8 +50,8 @@ const params = {
 };
 
 // # Variant 1: blocking result
-const result = await agentExecutor.invoke(params);
-console.log(result.output);
+// const result = await agentExecutor.invoke(params);
+// console.log(result.output);
 
 // # Variant 2: log streaming
 // import { filterLogStream, streamToLines } from "./streaming";
@@ -64,45 +64,45 @@ console.log(result.output);
 // }
 
 // # Variant 3: streaming events
-// const eventStream = agentExecutor.streamEvents(params, { version: "v1" });
+const eventStream = agentExecutor.streamEvents(params, { version: "v1" });
 
-// for await (const event of eventStream) {
-//   const eventType = event.event;
-//   if (eventType === "on_chain_start") {
-//     // Was assigned when creating the agent with `.withConfig({"runName": "Agent"})` above
-//     if (event.name === "Agent") {
-//       console.log("\n-----");
-//       console.log(
-//         `Starting agent: ${event.name} with input: ${JSON.stringify(
-//           event.data.input
-//         )}`
-//       );
-//     }
-//   } else if (eventType === "on_chain_end") {
-//     // Was assigned when creating the agent with `.withConfig({"runName": "Agent"})` above
-//     if (event.name === "Agent") {
-//       console.log("\n-----");
-//       console.log(`Finished agent: ${event.name}\n`);
-//       console.log(`Agent output was: ${event.data.output}`);
-//       console.log("\n-----");
-//     }
-//   } else if (eventType === "on_llm_stream") {
-//     const content = event.data?.chunk?.message?.content;
-//     // Empty content in the context of OpenAI means
-//     // that the model is asking for a tool to be invoked via function call.
-//     // So we only print non-empty content
-//     if (content !== undefined && content !== "") {
-//       process.stdout.write(content);
-//     }
-//   } else if (eventType === "on_tool_start") {
-//     console.log("\n-----");
-//     console.log(
-//       `Starting tool: ${event.name} with inputs: ${event.data.input}`
-//     );
-//   } else if (eventType === "on_tool_end") {
-//     // // console.log("\n-----");
-//     // // console.log(`Finished tool: ${event.name}\n`);
-//     // // console.log(`Tool output was: ${event.data.output}`);
-//     // console.log("\n-----");
-//   }
-// }
+for await (const event of eventStream) {
+  const eventType = event.event;
+  if (eventType === "on_chain_start") {
+    // Was assigned when creating the agent with `.withConfig({"runName": "Agent"})` above
+    if (event.name === "Agent") {
+      console.log("\n-----");
+      console.log(
+        `Starting agent: ${event.name} with input: ${JSON.stringify(
+          event.data.input
+        )}`
+      );
+    }
+  } else if (eventType === "on_chain_end") {
+    // Was assigned when creating the agent with `.withConfig({"runName": "Agent"})` above
+    if (event.name === "Agent") {
+      console.log("\n-----");
+      console.log(`Finished agent: ${event.name}\n`);
+      console.log(`Agent output was: ${event.data.output}`);
+      console.log("\n-----");
+    }
+  } else if (eventType === "on_llm_stream") {
+    const content = event.data?.chunk?.message?.content;
+    // Empty content in the context of OpenAI means
+    // that the model is asking for a tool to be invoked via function call.
+    // So we only print non-empty content
+    if (content !== undefined && content !== "") {
+      process.stdout.write(content);
+    }
+  } else if (eventType === "on_tool_start") {
+    console.log("\n-----");
+    console.log(
+      `Starting tool: ${event.name} with inputs: ${event.data.input}`
+    );
+  } else if (eventType === "on_tool_end") {
+    // // console.log("\n-----");
+    // // console.log(`Finished tool: ${event.name}\n`);
+    // // console.log(`Tool output was: ${event.data.output}`);
+    // console.log("\n-----");
+  }
+}
